326e104e7764daf0265e58005bea169f2e6e926a
jihye lim <tfj0531@gmail.com>
2019-04-21 17:13:48 +0900

    add

---
+++ importme/vsp.py
@@ -0,0 +1,45 @@
+'''
+Created on 2019. 4. 21.
+
+@author: jihye
+'''
+
+### 클러스터 결과를 담은 DataFrame과 사이킷런의 Cluster 객체등을 인자로 받아 클러스터링 결과를 시각화하는 함수
+ 
+def visualize_cluster_plot(clusterobj, dataframe, label_name, iscenter=True):
+    
+    import numpy as np
+    import matplotlib.pyplot as plt 
+    
+    
+    if iscenter :
+        centers = clusterobj.cluster_centers_
+        
+    unique_labels = np.unique(dataframe[label_name].values)
+    markers=['o', 's', '^', 'x', '*']
+    isNoise=False
+
+    for label in unique_labels:
+        label_cluster = dataframe[dataframe[label_name]==label]
+        if label == -1:
+            cluster_legend = 'Noise'
+            isNoise=True
+        else :
+            cluster_legend = 'Cluster '+str(label)
+        
+        plt.scatter(x=label_cluster['ftr1'], y=label_cluster['ftr2'], s=70,\
+                    edgecolor='k', marker=markers[label], label=cluster_legend)
+        
+        if iscenter:
+            center_x_y = centers[label]
+            plt.scatter(x=center_x_y[0], y=center_x_y[1], s=250, color='white',
+                        alpha=0.9, edgecolor='k', marker=markers[label])
+            plt.scatter(x=center_x_y[0], y=center_x_y[1], s=70, color='k',\
+                        edgecolor='k', marker='$%d$' % label)
+    if isNoise:
+        legend_loc='upper center'
+    else: legend_loc='upper right'
+    
+    plt.legend(loc=legend_loc)
+    plt.show()
+
